653ab6485a1f36bbcf280bd6c17e4afeeb8b4729
RiP3rQ <rafalpompa2000@gmail.com>
2023-12-28 22:13:30 +0100

    Additional guards settings + first e2e test + updated docker script + updated scripts  for tests + cleaning Db on initializing tests

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,7 @@
 import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
-import { Request } from 'express';
-import { GetUser } from 'src/auth/decorator';
-import { JwtGuard } from 'src/auth/guard';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
 
 @UseGuards(JwtGuard)
 @Controller('users')
