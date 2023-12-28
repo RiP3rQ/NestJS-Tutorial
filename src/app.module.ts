db6ca792e9dbe76d49666a0895b74c6c10477cc1
RiP3rQ <rafalpompa2000@gmail.com>
2023-12-28 18:35:59 +0100

    prisma basic setup in NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, BookmarkModule, AuthModule],
+  imports: [UserModule, BookmarkModule, AuthModule, PrismaModule],
 })
 export class AppModule {}
